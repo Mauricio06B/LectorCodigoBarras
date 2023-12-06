c37317ab2665e5d96b17e6ee9fa7c57c661194b2
mauricio bohorquez <mauricio.bohorquez13213@ucaldas.edu.co>
2023-12-05 20:01:50 -0500

    commit inicial

---
+++ app/src/main/java/com/mba/codigo_de_barras/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.mba.codigo_de_barras
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
+import com.mba.codigo_de_barras.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private  lateinit var  binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnScanner.setOnClickListener { initScanner()}
+    }
+
+    private fun initScanner() {
+        IntentIntegrator(this).initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if(result != null){
+            if(result.contents == null){
+                Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "El valor escaneado es: ${result.contents}", Toast.LENGTH_SHORT).show()
+            }
+        }
+        else{
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+}
